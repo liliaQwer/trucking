9a8214914092a00236144ecc312464d009efcca4
Handless8080 <vacya9@gmail.com>
2019-07-11 20:08:27 +0300

    Added custom login page example

--- web/src/main/java/com/Application.java
+++ web/src/main/java/com/Application.java
@@ -1,4 +1,4 @@
-package com.configs;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- web/src/main/java/com/configs/ResourceConfig.java
+++ web/src/main/java/com/configs/ResourceConfig.java
@@ -1,4 +1,13 @@
 package com.configs;
 
-public class resourceConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 }
